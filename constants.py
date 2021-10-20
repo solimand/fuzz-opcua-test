02fc7ffee0932c250f2401e5162c9d77dd78b959
solimand <m.solimand@gmail.com>
2021-10-20 17:01:09 +0300

    Definition of Hello Msg

--- constants.py
+++ constants.py
@@ -1,2 +1,11 @@
+# connection
 HOST_ADDR = "150.140.188.188"
-OPC_UA_PORT = 4841
+OPC_UA_PORT = 4840
+ENDPOINT_STRING = 'opc.tcp://localhost:4840/'.encode('utf-8')
+
+# msg types
+HELLO_MSG_TYPE = b'HEL'
+ACK_MSG_TYPE = b'ACK'
+OPEN_MSG_TYPE = b'OPN' 
+CLOSE_MSG_TYPE = b'CLO'
+MSG_TYPE = b'MSG'
--- my-opcua-fuzzer.py
+++ my-opcua-fuzzer.py
@@ -10,6 +10,33 @@ def main():
     target=Target(
         connection=TCPSocketConnection(constants.HOST_ADDR, constants.OPC_UA_PORT)))
 
+    #print("DBG: "+str(session.web_port))
+    print_dbg(session.web_port)
+    print_dbg("close")
+
+# -----------------------MSGs DEF---------------------
+# TODO use constants for msgs definitions
+def hello_definition():
+    s_initialize('Hello')
+
+    with s_block('h-header'):
+        s_bytes(b'HEL', name='Hello magic', fuzzable=False)
+        s_bytes(b'F', name='Chunk type', fuzzable=False)
+        s_size('h-body', offset=8, name='body size', fuzzable=False)
+
+    with s_block('h-body'):
+        s_dword(0, name='Protocol version')
+        s_dword(65536, name='Receive buffer size')
+        s_dword(65536, name='Send buffer size')
+        s_dword(0, name='Max message size')
+        s_dword(0, name='Max chunk count')
+        endpoint = ENDPOINT_STRING
+        s_dword(len(endpoint), name='Url length')
+        s_bytes(endpoint, name='Endpoint url')
+
+# -----------------------UTILS---------------------
+def print_dbg(msg):
+    print("DBG: "+str(msg))
 
 if __name__ == "__main__":
     main()
