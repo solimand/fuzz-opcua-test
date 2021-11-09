ffe4a59139ee93f0ebfb7b745aa2c0ad4ca494c4
solimand <m.solimand@gmail.com>
2021-11-09 12:48:06 +0200

    endpoints fuzz constants

--- msgDefinitions.py
+++ msgDefinitions.py
@@ -1,7 +1,9 @@
-from fuzzConstants import CLOSE_MSG_TYPE_ID, HELLO_MSG_BODY_NAME, ENDPOINT_STRING, CHUNK_TYPE
+from fuzzConstants import CLOSE_MSG_TYPE_ID, HELLO_MSG_BODY_NAME, ENDPOINT_STRING, CHUNK_TYPE, COMMON_MSG_TYPE
 from fuzzConstants import HELLO_MSG_NAME, HELLO_MSG_TYPE, HELLO_MSG_HEADER_NAME, HELLO_MSG_BODY_NAME
 from fuzzConstants import OPEN_MSG_NAME, OPEN_MSG_TYPE, OPEN_MSG_HEADER_NAME, OPEN_MSG_BODY_NAME, OPEN_MSG_SEC_POLICY_NONE
-from fuzzConstants import CLOSE_MSG_NAME, CLOSE_MSG_TYPE, CLOSE_MSG_HEADER_NAME, CLOSE_MSG_BODY_NAME, CLOSE_MSG_TYPE_ID, CLOSE_MSG_SEC_CH_ID_PRIM_NAME, CLOSE_MSG_SEC_TOKEN_ID_PRIM_NAME, CLOSE_MSG_SEC_SEQ_NUM_PRIM_NAME, CLOSE_MSG_SEC_REQ_ID_PRIM_NAME
+from fuzzConstants import CLOSE_MSG_NAME, CLOSE_MSG_TYPE, CLOSE_MSG_HEADER_NAME, CLOSE_MSG_BODY_NAME, CLOSE_MSG_TYPE_ID, SEC_CH_ID_PRIM_NAME, SEC_TOKEN_ID_PRIM_NAME, SEC_SEQ_NUM_PRIM_NAME, SEC_REQ_ID_PRIM_NAME
+from fuzzConstants import GET_ENDPOINTS_MSG_NAME, GET_ENDPOINTS_MSG_HEADER_NAME, GET_ENDPOINTS_MSG_BODY_NAME, GET_ENDPOINTS_MSG_TYPE_ID
+
 from fuzzConstants import UNIX_TIME
 
 from boofuzz import s_initialize, s_bytes, s_dword, s_block, s_size, s_qword
@@ -16,12 +18,13 @@ import struct
 # -----------------------UTILS---------------------
 def print_dbg(msg):
     print("\tDBG: "+str(msg))
+print_dbg.__doc__ = "Modified print func for debug and testing prints"
 
 def opcua_time():
     now = datetime.now()
     res_time = UNIX_TIME + (timegm(now.timetuple()) * 10000000)
     return res_time + (now.microsecond * 10) - 72000000000 #time correction
-
+opcua_time.__doc__ = "Get the time stamp in microseconds in OPC UA format"
 
 # -----------------------HELLO MSG---------------------
 def hello_msg_nf():
@@ -59,7 +62,6 @@ def hello_msg():
         s_dword(0, name='Max chunk count') #(700)
         s_dword(len(ENDPOINT_STRING), name='Url length') #(840)
         s_bytes(ENDPOINT_STRING, name='Endpoint url') #(2270)
-    
 
 # -----------------------OPEN MSG---------------------
 def open_msg():
@@ -142,10 +144,10 @@ def close_msg():
         s_size(CLOSE_MSG_BODY_NAME, offset=8, name='body size', fuzzable=False)
 
     with s_block(CLOSE_MSG_BODY_NAME):
-        s_dword(1, name=CLOSE_MSG_SEC_CH_ID_PRIM_NAME, fuzzable=False) #from open callback
-        s_dword(2, name=CLOSE_MSG_SEC_TOKEN_ID_PRIM_NAME, fuzzable=False) #from open callback
-        s_dword(3, name=CLOSE_MSG_SEC_SEQ_NUM_PRIM_NAME, fuzzable=False) #from open callback
-        s_dword(4, name=CLOSE_MSG_SEC_REQ_ID_PRIM_NAME, fuzzable=False) #from open callback
+        s_dword(1, name=SEC_CH_ID_PRIM_NAME, fuzzable=False) #from open callback
+        s_dword(2, name=SEC_TOKEN_ID_PRIM_NAME, fuzzable=False) #from open callback
+        s_dword(3, name=SEC_SEQ_NUM_PRIM_NAME, fuzzable=False) #from open callback
+        s_dword(4, name=SEC_REQ_ID_PRIM_NAME, fuzzable=False) #from open callback
         # type id  b'\x01\x00\xc4\x01'
         s_bytes(b'\x01\x00' + struct.pack('<H', CLOSE_MSG_TYPE_ID), name='Type id', fuzzable=False)
         # request header
@@ -160,28 +162,38 @@ def close_msg():
 
 
 # -----------------------GET ENDPOINTS MSG---------------------
-'''def get_endpoints():
+def get_endpoints_msg():
     s_initialize(GET_ENDPOINTS_MSG_NAME)
 
     with s_block(GET_ENDPOINTS_MSG_HEADER_NAME):
-        s_bytes(COMMON_MSG_TYPE, name='Get Endpoints', fuzzable=False)
+        s_bytes(COMMON_MSG_TYPE, name='Get endpoints', fuzzable=False)
         s_bytes(CHUNK_TYPE, name='Chunk type', fuzzable=False)
         s_size(GET_ENDPOINTS_MSG_BODY_NAME, offset=8, name='body size', fuzzable=False)
 
     with s_block(GET_ENDPOINTS_MSG_BODY_NAME):
-        s_dword(0, name='secure channel id', fuzzable=False)
-        s_dword(4, name='secure token id', fuzzable=False)
-        s_dword(2, name='secure sequence number', fuzzable=False)
-        s_dword(2, name='secure request id', fuzzable=False)
-        # Type ID
-        s_bytes(b'\x01\x00' + struct.pack('<H', 428), name='Type id', fuzzable=False)
-        # Req header
+        # security
+        s_dword(1, name=SEC_CH_ID_PRIM_NAME, fuzzable=False)  #from open callback
+        s_dword(2, name=SEC_TOKEN_ID_PRIM_NAME, fuzzable=False)  #from open callback
+        s_dword(3, name=SEC_SEQ_NUM_PRIM_NAME, fuzzable=False)  #from open callback
+        s_dword(4, name=SEC_REQ_ID_PRIM_NAME, fuzzable=False)  #from open callback
+
+
+        
+        # TODO define from here -> type id  b'\x01\x00... <-
+        s_bytes(b'\x01\x00' + struct.pack('<H', GET_ENDPOINTS_MSG_TYPE_ID), name='Type id', fuzzable=False)
+
+        # request header
         s_bytes(b'\x00\x00', name='authentication token')
-        s_qword(opcua_time(), name='timestamp')
+        s_qword(get_weird_opc_timestamp(), name='timestamp')
         s_dword(1, name='request handle')
         s_dword(0, name='return diagnostics')
         s_bytes(b'\xFF\xFF\xFF\xFF', name='audit entry id')
         s_dword(1000, name='timeout hint')
         s_bytes(b'\x00\x00\x00', name='additional header')
-        #Req params
-        #TODO other fields'''
+
+        # request parameter
+        endpoint = ENDPOINT_STRING
+        s_dword(len(endpoint), name='url length')
+        s_bytes(endpoint, name='endpoint url')
+        s_bytes(b'\xFF\xFF\xFF\xFF', name='locale ids')
+        s_bytes(b'\xFF\xFF\xFF\xFF', name='profile ids')
